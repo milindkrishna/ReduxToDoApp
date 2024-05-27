08db59b22db285ce4c2eba67996fd4d9f102fcce
Milind Krishna <54195987+milindkrishna@users.noreply.github.com>
2024-05-27 09:48:49 +0000

    updated code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-
+import AddTodos from './components/AddTodos'
+import Todos from './components/Todos'
 function App() {
 
   return (
     <>
       <h1>Learn about redux toolkit</h1>
+     <AddTodos/>
+     <Todos/>
     </>
   )
 }
